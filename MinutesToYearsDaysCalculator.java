fe7675f283c8438eefb217b5c49f8029a750eff4
Akilesh <akileshakileshs1234@gmail.com>
2022-06-03 17:20:04 +0530

    Add files via upload

---
+++ MinutesToYearsDaysCalculator.java
@@ -0,0 +1,19 @@
+package com.akilesh;
+
+public class MinutesToYearsDaysCalculator {
+
+    public static void printYearsAndDays(long minutes) {
+
+        // Validating the parameter
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long totalMinutesInOneDay = 24 * 60; // Total minutes in one day.
+            long totalNumberOfDays = minutes / totalMinutesInOneDay; // Finding number of days.
+            // Finding how many years will fit in the totalNumberOfDays and slicing the remainder.
+            long numberOfYears = totalNumberOfDays / 365;
+            long remainingDays = totalNumberOfDays % 365; // Adding the remaining days.
+            System.out.println(minutes + " min " + "= " + numberOfYears + " y " + "and " + remainingDays + " d");
+        }
+    }
+}
